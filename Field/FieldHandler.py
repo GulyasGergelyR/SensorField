193567b7ae5470a3f46ab312ae6e69a75b9e11f9
Unknown <g.gergo42@gmail.com>
2016-11-12 16:05:28 +0100

    working drawing

--- Field/FieldHandler.py
+++ Field/FieldHandler.py
@@ -1,18 +1,87 @@
 from collections import OrderedDict
+from tkinter import ALL
 
-size = 64
+size = 10
+cell_size = 20
+step_size = 1
+
+drawing_offset = 20
+
+class Selectable:
+    selected = []
+    only1allowed = False
+    selector_target = Cell.__name__
+
+    def __init__(self, color="#6984c9", selected_color="#3607ea"):
+        self._selected = False
+        self._color = color
+        self._selected_color = selected_color
+        self._width = 2
+        self._selected_width = 4
+
+    def select(self, b=True):
+        if self.only1allowed:
+            for s in self.selected:
+                s.selected = False
+            self.selected = []
+
+        if self.__class__.__name__ == self.selector_target and b:
+            self._selected = True
+            Selectable.selected += [self]
+        elif self in Selectable.selected:
+            self._selected = False
+            Selectable.selected.remove(self)
+
+    @property
+    def color(self):
+        if self._selected:
+            return self._selected_color
+        return self._color
+
+    @property
+    def width(self):
+        if self._selected:
+            return self._selected_width
+        return self._width
 
 
 class Field:
     def __init__(self):
-        self._corners = [[Corner(i, j) for i in range(size+1)] for j in range(size+1)]
-        self._cells = [[Cell(i, j, [self._corners[i+n//2][j+n % 2] for n in range(4)])
-                        for i in range(size)] for j in range(size)]
+        self._corners = [[Corner(i, j) for j in range(size+1)] for i in range(size+1)]
+        self._cells = [[Cell(i, j, [self._corners[i + n//2][j + n % 2] for n in [0, 1, 3, 2]])
+                        for j in range(size)] for i in range(size)]
         self._corners = []
         self._rooms = OrderedDict()
 
-    def add_room(self, room):
-        self._rooms[room.id] = room
+    @property
+    def cells(self):
+        return self._cells
+
+    @property
+    def rooms(self):
+        return self._rooms
+
+    def notify(self, event):
+        m_x = event.x
+        m_y = event.y
+        if Selectable.selector_target == Cell.__name__:
+            for row in self._cells:
+                for cell in row:
+                    if cell.point_is_inside(m_x, m_y):
+                        cell.select()
+        if Selectable.selector_target == Room.__name__:
+            for room in self._rooms.values():
+                if room.point_is_inside(m_x, m_y):
+                    room.select()
+
+    def create_new_room(self, cells):
+        r_id = 1
+        while r_id in self._rooms.keys():
+            r_id += 1
+        room = Room(self, r_id)
+        room.add_cells(cells)
+        self.init_walls_in(room)
+        self._rooms[r_id] = room
 
     def get_room(self, r_id):
         if r_id in self._rooms.keys():
@@ -23,46 +92,90 @@ class Field:
         self._create_walls()
         self._connect_walls()
 
+    def init_walls_in(self, room):
+        self._create_walls_in(room)
+        self._connect_walls_in(room)
+
     def _create_walls(self):
-        for r_id, room in self._rooms.iteritems():
-            for cell in room.cells:
-                # Check directions
-                p = cell.pos
-                # Up - 0, Right - 1, Down - 2, Left - 3
-                if p[0] > 0:
-                    if self._cells[p[0]][p[1]].room_id != self._cells[p[0]-1][p[1]].room_id:
-                        Wall(cell, 0)
-                if p[0] < size-1:
-                    if self._cells[p[0]][p[1]].room_id != self._cells[p[0]+1][p[1]].room_id:
-                        Wall(cell, 2)
-                if p[1] > 0:
-                    if self._cells[p[0]][p[1]].room_id != self._cells[p[0]][p[1]-1].room_id:
-                        Wall(cell, 0)
-                if p[1] < size-1:
-                    if self._cells[p[0]][p[1]].room_id != self._cells[p[0]][p[1]+1].room_id:
-                        Wall(cell, 2)
+        for room in self._rooms.values():
+            self._create_walls_in(room)
+
+    def _create_walls_in(self, room):
+        room.delete_walls()
+        for cell in room.cells:
+            # Check directions
+            p = cell.pos
+            # Up - 0, Right - 1, Down - 2, Left - 3
+
+            if p[0] > 0:
+                if self._cells[p[0]][p[1]].room_id != self._cells[p[0]-1][p[1]].room_id:
+                    Wall(cell, 0)
+            if p[0] < size-1:
+                if self._cells[p[0]][p[1]].room_id != self._cells[p[0]+1][p[1]].room_id:
+                    Wall(cell, 2)
+            if p[1] > 0:
+                if self._cells[p[0]][p[1]].room_id != self._cells[p[0]][p[1]-1].room_id:
+                    Wall(cell, 3)
+            if p[1] < size-1:
+                if self._cells[p[0]][p[1]].room_id != self._cells[p[0]][p[1]+1].room_id:
+                    Wall(cell, 1)
 
     def _connect_walls(self):
         for room in self._rooms:
+            self._connect_walls_in(room)
+
+    def _connect_walls_in(self, room):
+        for wall in room.walls:
+            for w in room.walls:
+                if wall != w:
+                    if wall.try_to_connect(w):
+                        break
+
+    def draw(self, canvas):
+        canvas.delete(ALL)
+        # Draw cells
+        self._draw_cells(canvas)
+        # Draw grid and walls
+        self._draw_grid_and_walls(canvas)
+
+    def _draw_cells(self, canvas):
+        for row in self._cells:
+            for cell in row:
+                cell.draw(canvas)
+
+    def _draw_grid_and_walls(self, canvas):
+        x = drawing_offset
+        y = drawing_offset
+        for i in range(size+1):
+            canvas.create_line(x + i * cell_size, y, x + i * cell_size, y+size*cell_size, fill="#1f5a56")
+            canvas.create_line(x, y + i * cell_size, x + size * cell_size, y + i * cell_size, fill="#1f5a56")
+
+        for room in self._rooms.values():
             for wall in room.walls:
-                for w in room.walls:
-                    if wall != w:
-                        if wall.try_to_connect(w):
-                            break
+                wall.draw(canvas)
 
 
-class Room:
-    def __init__(self, new_id):
+class Room(Selectable):
+    def __init__(self, field, new_id, color="#000066", selected_color="#0066ff"):
+        super(Room, self).__init__(color, selected_color)
         self._id = new_id
         self._walls = []
         self._cells = []
+        self._field = field
 
     def add_wall(self, wall):
         self._walls += [wall]
 
-    def add_cell(self, cell):
-        self._cells += [cell]
-        cell.room = self
+    def add_cells(self, cells):
+        # cells has to be a tuple of Cell or [int, int]
+        for cell in cells:
+            if isinstance(cell, (tuple, list)):
+                cell = self._field.cells[cell[0]][cell[1]]
+            if isinstance(cell, Cell):
+                self._cells += [cell]
+                cell.room = self
+            else:
+                raise TypeError("Wrong type from cell")
 
     @property
     def cells(self):
@@ -76,10 +189,17 @@ class Room:
     def id(self):
         return self._id
 
+    def delete_walls(self):
+        self._walls = []
 
-class Cell:
-    def __init__(self, i, j, corners):
-        self._room_id = 0
+    def point_is_inside(self, m_x, m_y):
+        return any([cell.point_is_inside(m_x, m_y)] for cell in self._cells)
+
+
+class Cell(Selectable):
+    def __init__(self, i, j, corners, color="#006666", selected_color="#0066ff"):
+        super(Cell, self).__init__(color, selected_color)
+        self._room_id = -1
         self._room = None
         self._pos = [i, j]
         self._sides = [None, None, None, None]
@@ -90,7 +210,18 @@ class Cell:
 
     def get_corners_at(self, o):
         # Left upper - 0, Right upper - 1, Right bottom - 2, Left bottom - 3
-        return [self._corners[o], self._corners[(o+1) % 3]]
+        return [self._corners[o], self._corners[(o+1) % 4]]
+
+    def draw(self, canvas):
+        x = drawing_offset + self._pos[1] * cell_size
+        y = drawing_offset + self._pos[0] * cell_size
+        canvas.create_rectangle(x, y, x+cell_size, y+cell_size, fill=self.color)
+
+    def point_is_inside(self, m_x, m_y):
+        x = drawing_offset + self._pos[1] * cell_size
+        y = drawing_offset + self._pos[0] * cell_size
+
+        return x < m_x < x+cell_size and y < m_y < y+ cell_size
 
     @property
     def room_id(self):
@@ -107,20 +238,32 @@ class Cell:
     @room.setter
     def room(self, room):
         self._room = room
+        print("id: "+str(room.id))
         self._room_id = room.id
+
+    @property
+    def color(self):
+        if self._selected:
+            return self._selected_color
+        elif self._room is not None:
+            return self._room.color
+        return self._color
 
 
 class Corner:
     def __init__(self, i, j):
         self._pos = [i, j]
 
+    @property
+    def pos(self):
+        return self._pos
 
-class Wall:
+
+class Wall(Selectable):
     def __init__(self, cell, o):
-        self._cell = cell
+        super(Wall, self).__init__(color="#009900", selected_color="#00ff00")
         self._orientation = o
         self._corners = cell.get_corners_at(o)
-        cell.add_wall(self)
         cell.room.add_wall(self)
         self._e2l = None  # element to the left
         self._e2r = None  # element to the right
@@ -154,6 +297,13 @@ class Wall:
     @property
     def e2r(self):
         return self._e2r
+
+    def draw(self, canvas):
+        x1 = drawing_offset + self._corners[0].pos[1] * cell_size
+        y1 = drawing_offset + self._corners[0].pos[0] * cell_size
+        x2 = drawing_offset + self._corners[1].pos[1] * cell_size
+        y2 = drawing_offset + self._corners[1].pos[0] * cell_size
+        canvas.create_line(x1, y1, x2, y2, fill=self.color, width=self.width)
 
 
 class Door:
--- Main/GUIhandler.py
+++ Main/GUIhandler.py
@@ -1,80 +1,108 @@
 from tkinter import *
 from tkinter import ttk
 
-
-root = Tk()
-
-def donothing():
-    filewin = Toplevel(root)
-    button = Button(filewin, text="Do nothing button")
-    button.pack()
+from Field.FieldHandler import Field
 
 
-def init_properties():
-    root.wm_title("SensorField v0.1")
-    #root.resizable(width=False, height=False)
+class GUIHandler:
 
+    def __init__(self):
+        self._root = Tk()
+        self._canvas = None
+        self._init_properties()
+        self._create_menu()
+        self._create_main_frames(self._root)
 
-def create_menu():
-    menubar = Menu(root)
-    filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="New", command=donothing)
-    filemenu.add_command(label="Open", command=donothing)
-    filemenu.add_command(label="Save", command=donothing)
-    filemenu.add_command(label="Save as...", command=donothing)
-    filemenu.add_command(label="Close", command=donothing)
+        self._field = None
 
-    filemenu.add_separator()
+    @staticmethod
+    def _do_nothing():
+        pass
+        """
+        filewin = Toplevel(self._root)
+        button = Button(filewin, text="Do nothing button")
+        button.pack()
+        """
 
-    filemenu.add_command(label="Exit", command=root.quit)
-    menubar.add_cascade(label="File", menu=filemenu)
-    editmenu = Menu(menubar, tearoff=0)
-    editmenu.add_command(label="Undo", command=donothing)
+    def _init_properties(self):
+        self._root.wm_title("SensorField v0.1")
 
-    editmenu.add_separator()
+    def _create_menu(self):
+        menubar = Menu(self._root)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New", command=self._do_nothing)
+        filemenu.add_command(label="Open", command=self._do_nothing)
+        filemenu.add_command(label="Save", command=self._do_nothing)
+        filemenu.add_command(label="Save as...", command=self._do_nothing)
+        filemenu.add_command(label="Close", command=self._do_nothing)
 
-    editmenu.add_command(label="Cut", command=donothing)
-    editmenu.add_command(label="Copy", command=donothing)
-    editmenu.add_command(label="Paste", command=donothing)
-    editmenu.add_command(label="Delete", command=donothing)
-    editmenu.add_command(label="Select All", command=donothing)
+        filemenu.add_separator()
 
-    menubar.add_cascade(label="Edit", menu=editmenu)
-    helpmenu = Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="Help Index", command=donothing)
-    helpmenu.add_command(label="About...", command=donothing)
-    menubar.add_cascade(label="Help", menu=helpmenu)
+        filemenu.add_command(label="Exit", command=self._root.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        editmenu = Menu(menubar, tearoff=0)
+        editmenu.add_command(label="Undo", command=self._do_nothing)
 
-    root.config(menu=menubar)
+        editmenu.add_separator()
 
+        editmenu.add_command(label="Cut", command=self._do_nothing)
+        editmenu.add_command(label="Copy", command=self._do_nothing)
+        editmenu.add_command(label="Paste", command=self._do_nothing)
+        editmenu.add_command(label="Delete", command=self._do_nothing)
+        editmenu.add_command(label="Select All", command=self._do_nothing)
 
-def run():
-    root.mainloop()
+        menubar.add_cascade(label="Edit", menu=editmenu)
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="Help Index", command=self._do_nothing)
+        helpmenu.add_command(label="About...", command=self._do_nothing)
+        menubar.add_cascade(label="Help", menu=helpmenu)
 
+        self._root.config(menu=menubar)
 
-def create_main_frames(parent):
-    canvas = Canvas(parent, width=500, height=500)
-    tabs = create_tabs(parent)
+    def run(self):
+        self.draw()
+        self._root.mainloop()
 
-    canvas.grid(row=0, column=0, rowspan=1, columnspan=1)
-    tabs.grid(row=0, column=1, rowspan=1, columnspan=1, sticky=(N, S, W, E))
+    def _canvas_mouse_callback(self, event):
+        print("x:"+str(event.x))
+        print("y:"+str(event.y))
+        if self._field is not None:
+            self._field.notify(event)
 
-    parent.columnconfigure(0, weight=5)
-    parent.columnconfigure(1, weight=5)
-    parent.rowconfigure(0, weight=1)
+    def _create_main_frames(self, parent):
+        self._canvas = Canvas(parent, width=500, height=500)
+        self._canvas.bind("<Button-1>", self._canvas_mouse_callback)
+        tabs = self._create_tabs(parent)
 
+        self._canvas.grid(row=0, column=0, rowspan=1, columnspan=1)
+        tabs.grid(row=0, column=1, rowspan=1, columnspan=1, sticky=(N, S, W, E))
 
-def create_tabs(parent):
-    n = ttk.Notebook(parent)
-    f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
-    f2 = ttk.Frame(n)   # second page
+        parent.columnconfigure(0, weight=5)
+        parent.columnconfigure(1, weight=5)
+        parent.rowconfigure(0, weight=1)
 
-    n.add(f1, text='One')
-    n.add(f2, text='Two')
-    return n
+    @staticmethod
+    def _create_tabs(parent):
+        n = ttk.Notebook(parent)
+        f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
+        f2 = ttk.Frame(n)   # second page
+
+        n.add(f1, text='One')
+        n.add(f2, text='Two')
+        return n
+
+    def add_field(self, field):
+        self._field = field
+
+    def draw(self):
+        # Draw cells
+        self._field.draw(self._canvas)
 
 if __name__ == "__main__":
-    init_properties()
-    create_menu()
-    create_main_frames(root)
-    run()
+    guihandler = GUIHandler()
+    field = Field()
+    field.create_new_room([[2, 2], [2, 3], [3, 3]])
+    field.create_new_room([[1, 1], [1, 2], [1, 3]])
+    guihandler.add_field(field)
+    guihandler.run()
+
